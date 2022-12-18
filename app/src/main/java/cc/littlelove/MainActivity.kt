9847e82d5442278c0865579738c043107fe0b8f6
kevinw <kevinw-m@qq.com>
2022-12-18 10:36:50 +0800

    first commit.

---
+++ app/src/main/java/cc/littlelove/MainActivity.kt
@@ -0,0 +1,11 @@
+package cc.littlelove
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
